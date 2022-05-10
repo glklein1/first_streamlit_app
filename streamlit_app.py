7b97c675d78d88321ece7cf0d3625b2266d2ed56
glklein1 <103961862+glklein1@users.noreply.github.com>
2022-05-10 12:49:07 -0400

    Update streamlit_app.py
    updates for function add

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # being replaced with this
 streamlit.dataframe(fruits_to_show)
 
+# create repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     return fruityvice_normalized    
+
 #New Section to display FruityVice API Response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -34,13 +40,18 @@ try:
     if not fruit_choice:
       streamlit.error("Please select a fruit to get more information.")
     else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruity_vice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
+    
+#    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#      fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
+#      streamlit.dataframe(fruityvice_normalized)
       
-except URLError as e:
-  streamlit.error()
-      
+# except URLError as e:
+#  streamlit.error()
+
+
+
 streamlit.write ('The user entered ', fruit_choice)
 
 # import requests
